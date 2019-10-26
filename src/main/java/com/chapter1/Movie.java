714f3323a0e4d470d4392d4676fcaeb8222b6745
Ashis Jena <ashisjena.talk2u@gmail.com>
2019-10-26 23:38:36 +0530

    Chapter1: Moving getCharge and getFrequentRenterPoints from Rental to Movie. As it depends more on movie properties. And movie property more likely to change.

--- src/main/java/com/chapter1/Movie.java
+++ src/main/java/com/chapter1/Movie.java
@@ -28,4 +28,34 @@ public class Movie {
   public void setPriceCode(int priceCode) {
     this.priceCode = priceCode;
   }
+
+  public double getCharge(int daysRented) {
+    double result = 0;
+    switch (this.getPriceCode()) {
+      case Movie.REGULAR:
+        result += 2;
+        if (daysRented > 2) {
+          result += (daysRented - 2) * 1.5;
+        }
+        break;
+      case Movie.NEW_RELEASE:
+        result += daysRented * 3;
+        break;
+      case Movie.CHILDREN:
+        result += 1.5;
+        if (daysRented > 3) {
+          result += (daysRented - 3) * 1.5;
+        }
+        break;
+    }
+    return result;
+  }
+
+  public int getFrequentRenterPoints(int daysRented) {
+    if ((this.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+      return 2;
+    } else {
+      return 1;
+    }
+  }
 }
